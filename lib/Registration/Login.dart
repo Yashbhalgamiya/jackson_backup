58f417e4bcea99d8895f571d443520ad2d15c942
Yashbhalgamiya <86512736+Yashbhalgamiya@users.noreply.github.com>
2022-08-17 14:06:50 +0530

    Login Page 1.0

--- lib/Registration/Login.dart
+++ lib/Registration/Login.dart
@@ -9,16 +9,85 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          child:Column(
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/login.png'),
+          fit: BoxFit.cover
+        )
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Stack(
             children: [
-              Text(
-                "This is Login Page",
-                style: TextStyle(
-                    fontSize: 50
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 300.0),
+                  height: 250,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))
+                  ),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 10.0,top: 10.0,right: 10.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            hintText: 'Username' ,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            hintText: 'Password',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                                'Sign In',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 27,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundColor: Colors.blue,
+                              child: IconButton(
+                                color: Colors.white,
+                                onPressed: (){},
+                                icon: Icon(Icons.arrow_forward),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                  ),
                 ),
+
               ),
             ],
           ),
