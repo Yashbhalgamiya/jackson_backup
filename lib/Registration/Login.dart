506d77383ef670c620d34372f6618450d83457f9
Yashbhalgamiya <86512736+Yashbhalgamiya@users.noreply.github.com>
2022-08-25 10:39:50 +0530

    Login Page 1.0

--- lib/Registration/Login.dart
+++ lib/Registration/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jackson_app/home.dart';
+
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -11,11 +13,8 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('images/login.png'),
-          fit: BoxFit.cover
-        )
-      ),
+          image: DecorationImage(
+              image: AssetImage('images/login.png'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SafeArea(
@@ -28,66 +27,60 @@ class _LoginState extends State<Login> {
                   width: 300,
                   decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))
-                  ),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
                   child: Container(
-                    margin: EdgeInsets.only(left: 10.0,top: 10.0,right: 10.0),
-                    child: Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            hintText: 'Username' ,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            )
+                      margin:
+                          EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: 'Username',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
                           ),
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            hintText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10)
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: 'Password',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                                'Sign In',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 27,
-                                fontWeight: FontWeight.w700,
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Center(
+                            child: MaterialButton(
+                              color: Colors.blue,
+                              onPressed: (){
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context)=>Home())
+                                );
+                              },
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.white
+                                ),
                               ),
-                            ),
-                            CircleAvatar(
-                              radius: 30.0,
-                              backgroundColor: Colors.blue,
-                              child: IconButton(
-                                color: Colors.white,
-                                onPressed: (){},
-                                icon: Icon(Icons.arrow_forward),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)
                               ),
                             )
-                          ],
-                        ),
-                      ],
-                    )
-                  ),
+                          )
+                        ],
+                      )),
                 ),
-
               ),
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jackson_app/Registration/Login.dart';
+import 'package:jackson_app/home.dart';
 
 void main() {
   runApp(MaterialApp(
